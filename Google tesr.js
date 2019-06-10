8e11a4ad9343f9fbb3ec2d8ee561db8aa3325afa
Alex <40590201+sairkuk@users.noreply.github.com>
2019-06-10 10:23:20 +0300

    Delete Google tesr.js

--- Google tesr.js
+++
@@ -1,5 +0,0 @@
-var webdriver = require('selenium-webdriver');
-
-var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
-
-driver.get('https://rg-mechanics.org');
